2f52d92b08cbafb6d99bc3c2ac0d9c9a7e927b72
Mukkupretski <mukkupretski@gmail.com>
2024-03-22 11:38:34 +0200

    Exclude .js

--- socket.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.socket = void 0;
-const socket_io_client_1 = require("socket.io-client");
-const settings_1 = require("./settings");
-exports.socket = (0, socket_io_client_1.io)(settings_1.settings.localhost
-    ? "http://localhost:3000"
-    : "https://w8b2j173-3000.euw.devtunnels.ms/");
