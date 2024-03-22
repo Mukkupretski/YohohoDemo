f2d69b68956607f41423d01adbbb62dedd6c082a
Mukkupretski <mukkupretski@gmail.com>
2024-03-22 11:52:00 +0200

    Dd stuff

--- Utils/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NO_RENDER_COLOR = exports.GRASSPATCH_CENTER = exports.MAP_COLOR = void 0;
-exports.MAP_COLOR = "green";
-exports.GRASSPATCH_CENTER = "lime";
-exports.NO_RENDER_COLOR = "black";
